9cf2dd3b5b8d634afa1473beb5268f044470ab5b
Cardandom <carlosddominguezc@gmail.com>
2026-08-01 08:06:45 -0500

    chore: remove Vite entrypoint

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './vite-fonts.css';
-import './index.css';
-
-createRoot(document.getElementById('root')!).render(
-  <App />
-);
